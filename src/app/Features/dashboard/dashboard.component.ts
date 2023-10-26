7eb6971ef81030aeee5100258947d1b33f0ef39e
whimofgod <aaronkolins98@gmail.com>
2023-10-26 11:34:41 +0200

    transforming dashboard compent, adding style card, designing table style

--- src/app/Features/dashboard/dashboard.component.ts
+++ src/app/Features/dashboard/dashboard.component.ts
@@ -1,18 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent implements OnInit {
-  pageTitle: string = '';
+export class DashboardComponent {
 
-  constructor(private route: ActivatedRoute) { }
-
-  ngOnInit() {
-    // Récupérez le libellé de la page à partir des données de la route active
-    this.pageTitle = this.route.snapshot.data['title'];
-  }
 }
